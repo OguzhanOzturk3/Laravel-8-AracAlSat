ce8413bb34c34753c16f164c7f475da14138f5c7
oguzhanozturk3@hotmail.com <oguzhanozturk3@hotmail.com>
2021-11-12 17:30:37 +0300

    Controller View

---
+++ resources/views/home/test.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Laravel Test Page</title>
+</head>
+<body>
+
+<h1>Laravel Test</h1>
+Id no : {{$id}} <br>
+Nmae : {{$name}}
+<br>
+<?php
+echo "Id Number:", $id;
+echo "<br>Name: ",$name;
+
+for ($i=1;$i<=$id;$i++){
+echo "<br> $i $name";
+}
+?>
+<a href = "{{route('home')}}">Ana sayfa</a>
+</body>
+</html>
