89ba3882549685e034d08da2b51f4d034a8eb99e
oguzhanozturk3@hotmail.com <oguzhanozturk3@hotmail.com>
2021-12-04 18:18:56 +0300

    Bootsrap Layout

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.home')
+
+@section('title', 'Sub Page Title')
+
+@section('content')
+    <div class="col-sm-12">
+        <h1>About Us</h1>
+        aaaaaaaaaaaaa
+        <br>
+        bbbbbbbbbbbbbbbbbbbbbbbbb<br>
+        ccccccccccccccccc<br>
+    </div>
+@endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,13 +1,31 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>First Laravel Page</title>
-</head>
-<body>
+@extends('layouts.home')
 
-<h1>Welcome to Laravel</h1>
-<p>This is a paragraph.</p>
-<br>
-<a href = "{{route('test', ['id' => 12, 'name' => 'Fatma'])}}">Test Sayfası</a>
-</body>
-</html>
+@section('title', 'Sub Page Title')
+
+@section('sidebar')
+    <div class="col-sm-3 col-lg-2" style = "background-color: #fde8e8">
+
+    <ul>
+        <li>Bilgisayar</li>
+        <li>Elektronik</li>
+        <li>Ev eşya</li>
+
+    </ul>
+
+        </div>
+@endsection
+
+@section('content')
+    <div class="col-sm-9 col-lg-10">
+            <h1>Silder</h1>
+        zzzzzzzzzzzzzzz
+    <br>
+    bbbbbbbbbbbbb <br>
+
+            <h1>
+                İçerikleri
+            </h1>
+    dddddddddddd<br>
+    aaaaaaaaaaa<br>
+</div>
+@endsection
