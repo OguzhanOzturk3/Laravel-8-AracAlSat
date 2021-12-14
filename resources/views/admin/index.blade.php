0fa57746c41a91e3875c9ed411ec5bdb7613ba89
oguzhanozturk3@hotmail.com <oguzhanozturk3@hotmail.com>
2021-12-14 16:53:19 +0300

    Admin Login

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.admin')
 
 @section('title', 'Admin Panel Home Page')
-
+@include('admin._header')
+@include('admin._sidebar')
+@include('admin._headerDesktop')
 @section('content')
     @include('admin._content')
 @endsection
+@include('admin._footer')
