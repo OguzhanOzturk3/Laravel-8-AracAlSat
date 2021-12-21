f1c416cc6b10fd964f32f08fe71cef420d85194f
oguzhanozturk3@hotmail.com <oguzhanozturk3@hotmail.com>
2021-12-21 19:31:20 +0300

    Category Database İşlemleri

---
+++ resources/views/admin/_blank.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.admin')
+
+@section('title', 'Admin Panel Home Page')
+@include('admin._header')
+@include('admin._sidebar')
+@include('admin._headerDesktop')
+<div class="page-container">
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Typography</strong>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="typo-headers">
+                                <h1 class="pb-2 display-4">Very Important Information H1</h1>
+                                <h2 class="pb-2 display-5">Sections & Modal Names H2</h2>
+                                <h3 class="pb-2 display-5">Articles & Block Headings H3</h3>
+                                <h4 class="pb-2 display-5">Random Tiny Heading H4</h4>
+                                <h5 class="pb-2 display-5">Random Tiny Heading H5</h5>
+                                <h6 class="pb-4 display-5">Random Tiny Heading H6</h6>
+                            </div>
+                            <div class="typo-articles">
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+@include('admin._footer')
+
---
+++ resources/views/admin/category.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.admin')
+
+@section('title', 'Category List')
+@include('admin._header')
+@include('admin._sidebar')
+@include('admin._headerDesktop')
+<div class="page-container">
+<!-- MAIN CONTENT-->
+<div class="main-content">
+    <div class="section__content section__content--p30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Category</strong>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="col-lg-12">
+                                <div class="table-responsive table--no-card m-b-30">
+                                    <table class="table table-borderless table-striped table-earning">
+                                        <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Parent</th>
+                                            <th>Title(s)</th>
+                                            <th>Status</th>
+                                            <th>Edit</th>
+                                            <th>Delete</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($datalist as $rs)
+                                            <p></p>
+
+                                        <tr>
+                                            <td>{{$rs->id}}</td>
+                                            <td>{{$rs->parent_id}}</td>
+                                            <td>{{$rs->title}}</td>
+                                            <td>{{$rs->status}}</td>
+                                            <td>Edit</td>
+                                            <td>Delete</td>
+                                        </tr>
+                                        @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+@section('footer')
+    <script src="{{asset('assets')}}/admin/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <!-- Jquery JS-->
+    <script src="{{asset('assets')}}/admin/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{asset('assets')}}/admin/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="{{asset('assets')}}/admin/vendor/slick/slick.min.js">
+    </script>
+    <script src="{{asset('assets')}}/admin/vendor/wow/wow.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/animsition/animsition.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="{{asset('assets')}}/admin/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="{{asset('assets')}}/admin/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="{{asset('assets')}}/admin/vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="js/main.js"></script>
+@endsection
+@include('admin._footer')
+
